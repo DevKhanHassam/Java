73669c3d12e509d4a6cb8cd107645cd8d0c4bc97
Hassam Khan <68502565+DevKhanHassam@users.noreply.github.com>
2023-10-30 13:23:25 +0100

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class check{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+      
+      
+
+        int [] num={5,4,3,3,2,1};
+
+
+        for(int i=0;i<num.length;i++)
+        {
+            for(int j=0; j<(num.length-i-1);j++)
+        {
+            if(num[j]>num[j+1])
+            {
+                int temp = num[j];
+                num[j]=num[j+1];
+                num[j+1]=temp;
+            }
+
+            
+        }   
+        }
+        for(int i=0;i<num.length;i++)
+        {
+            System.out.print(num[i]+",");
+        }
+    }
+}
