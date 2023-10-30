3804013ec7e287f26e28943c948c3691abf704c4
Hassam Khan <68502565+DevKhanHassam@users.noreply.github.com>
2023-10-30 15:44:17 +0100

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -8,7 +8,7 @@ public class check{
         int [] num={5,4,3,3,2,1};
 
 
-        for(int i=0;i<num.length;i++)
+        for(int i=0;i<num.length-1;i++)
         {
             for(int j=0; j<(num.length-i-1);j++)
         {
